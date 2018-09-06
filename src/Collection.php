85b4ea3822469d2f9f649089dacda82c2d2e5a49
Abraão Zaidan <abraao@cakeweb.com.br>
2018-09-06 20:59:13 -0300

    Melhoria
    Suporte à paginação.

--- src/Collection.php
+++ src/Collection.php
@@ -131,6 +131,120 @@ abstract class Collection extends \MongoDB\Collection
 		return parent::aggregate($pipeline, $options);
 	}
 
+	protected function _findAndPaginate(array $filters): ?array
+	{
+		return $this->selectIds($filters);
+	}
+
+	// Esse método foi deixado no código-fonte para retrocompatibilidade
+	// Novas implementações devem sobrescrever apenas o método _findAndPaginate
+	public function selectIds(array $filters): ?array
+	{
+		return null;
+	}
+
+	final public function findAndPaginate(?array $filters = null, ?array $sort = null, ?int $page = null, ?int $perPage = null, ?array $pipeline = null): array
+	{
+		$query = \Flight::request()->query;
+		if(is_null($filters))
+		{
+			// Obtém todos os filtros
+			$filters = empty($query->filter) || $query->filter == '{}'
+				? []
+				: json_decode($query->filter, true);
+		}
+		if(is_null($sort))
+		{
+			// Obtém a ordenação desejada
+			$sort = empty($query->sort) || $query->sort == '{}'
+				? []
+				: json_decode($query->sort, true);
+		}
+		if(is_null($page))
+		{
+			// Obtém a página desejada
+			$page = (int)$query->page ?: 1;
+		}
+		if(is_null($perPage))
+		{
+			// Obtém a quantidade desejada
+			$perPage = (int)$query->perPage ?: 12;
+		}
+
+		// Obtém todos os IDs que satisfazem os $filters informados
+		$hasFilters = !empty($filters);
+		if(isset($filters['search']) && $filters['search'] == '' && count($filters) == 1)
+		{
+			$hasFilters = false;
+		}
+		if($hasFilters)
+		{
+			if(!$ids = $this->_findAndPaginate($filters))
+			{
+				return [
+					'currentPageItems' => [],
+					'totalItems'=> 0,
+					'perPage'=> $perPage,
+					'page' => $page
+				];
+			}
+			$match = [
+				'_id' => [
+					'$in' => $ids
+				]
+			];
+		}
+		else
+		{
+			$match = [];
+		}
+
+		// Obtém apenas os itens da página atual
+		$sort = array_map('intval', $sort);
+		if(!array_key_exists('_id', $sort))
+		{
+			$sort['_id'] = 1;
+		}
+		$skip = ($page - 1) * $perPage;
+		if($pipeline)
+		{
+			if($hasFilters)
+			{
+				array_unshift($pipeline, [
+					'$match' => $match
+				]);
+			}
+			array_push($pipeline, [
+				'$sort' => $sort
+			], [
+				'$limit' => $skip + $perPage // When a $sort immediately precedes a $limit in the pipeline, the $sort operation only maintains the top n results as it progresses, where n is the specified limit, and MongoDB only needs to store n items in memory.
+			], [
+				'$skip' => $skip
+			]);
+			$currentPageItems = $this->aggregate($pipeline)->toArray();
+		}
+		else
+		{
+			$currentPageItems = $this->find($match, [
+				'sort' => $sort,
+				'skip' => $skip,
+				'limit' => $perPage
+			])->toArray();
+		}
+
+		// O CakePaginator exige um retorno com as propriedades:
+		// - currentPageItems
+		// - totalItems
+		// - perPage
+		// - page
+		return [
+			'currentPageItems' => $currentPageItems,
+			'totalItems'=> count($currentPageItems),
+			'perPage'=> $perPage,
+			'page' => $page
+		];
+	}
+
 	final public function varExport($var, bool $return = false, int $tabs = 0)
 	{
 		if(is_array($var))
